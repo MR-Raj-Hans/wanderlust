a0c5bf0f3d24b6fd00d8dea4ba8b4ce5320ca69a
MR-Raj-Hans <rajhans1501@gmail.com>
2026-06-23 18:16:34 +0530

    updated project

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Listing = require('./models/listing.js')
 
 const MONGO_URL='mongodb://127.0.0.1:27017/Wanderpark';
 
@@ -13,13 +14,25 @@ main()
  });
 
 async function main(){
-    await mongoose.connect(MONGO_URL);
+     await mongoose.connect(MONGO_URL);
 }
 
 app.get("/",(req,res)=>{
     res.send("hello i am groot");
 });
- 
+app.get("/testlisting", async (req,res)=>{
+    let samplelisting = new Listing({
+        title: " awseome villa",
+        description: "this is a beautiful villa",
+        price: 10000,
+        country: "India",
+        location:"banaras" 
+     });
+     await samplelisting.save();
+     console.log("sample listing saved");
+     res.send("sucefully saved"); 
+});
+
 app.listen(8080, ()=>{
     console.log("serveer is running on port 8080");
 }); 
--- models/listing.js
+++ models/listing.js
@@ -1,13 +1,23 @@
-const mongoose = required('mongoose');
-const Schema= mongoode.Schemal;
+const mongoose = require('mongoose');
+const Schema= mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: String,
-    description: String,
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        set : (value) =>
+            value=== " "
+                ? "https://images.unsplash.com/photo-1532664189809-02133fee698d?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                :value,
+    },
     image: String,
-    prince: Number,
+    price: Number,
     location: String,
     country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
