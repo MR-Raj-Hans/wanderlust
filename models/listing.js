32e679e334fee6f33ba7b4f09f18b052ebcc143b
MR-Raj-Hans <rajhans1501@gmail.com>
2026-06-23 18:46:12 +0530

    updated project

--- models/listing.js
+++ models/listing.js
@@ -6,14 +6,16 @@ const listingSchema = new Schema({
         type: String,
         required: true,
     },
-    description: {
+    description: String,
+     image: {
         type: String,
-        set : (value) =>
-            value=== " "
+        default:
+             "https://images.unsplash.com/photo-1532664189809-02133fee698d?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set : (v) =>
+            v=== ""
                 ? "https://images.unsplash.com/photo-1532664189809-02133fee698d?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                :value,
+                : v,
     },
-    image: String,
     price: Number,
     location: String,
     country: String,
